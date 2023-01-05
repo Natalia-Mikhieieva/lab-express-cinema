0ebead733aa4356dd9d5ceff132cb2b0808d6081
Natalia Gravereaux <natalia.gravereaux@ose-engineering.fr>
2023-01-05 22:33:51 +0100

    iterations 1 & 2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const express = require("express");
+const Movie = require("..models/Movies.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
-module.exports = router;
+/* GET all movies */
+router.get("/movies", (res, req, next) => {
+  Movie.find()
+    .select({ title: 1, image: 1 })
+    .then(moviesFromDB => {
+      res.render("movies", { moviesFromDB })
+    })
+    .catch(err => console.log)
+});
+
+/* GET MOVIES page 
+router.get("movies/:movieId", (req, res) => {
+  console.log(req.params);
+  Movie.findById(req.params.bookId).then((result) => {
+    res.render("/movies", result);
+  });
+});
+module.exports = router;*/
